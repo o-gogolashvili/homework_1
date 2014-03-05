028d8b1c073fd740e16da4a90b86060b73502fb2
Otar Gogolashvili <otar@Omedia-Staging.(none)>
2014-03-05 13:03:42 +0000

    array

---
+++ agent.php
@@ -0,0 +1,16 @@
+<?php
+$ch=curl_init();
+
+curl_setopt($ch, CURLOPT_URL, "http://livescore.com/");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE); 
+
+$result = curl_exec($ch);
+//print $result;
+curl_close($ch);
+
+preg_match ('/<table class="league-table">+.+<\/table>/',$result, $matches);
+print $matches;
+
+?>
+ 
